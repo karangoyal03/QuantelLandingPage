bb7fb8cbe7b48a2138f639300768bb43d6e58ae1
karangoyal03 <49104496+karangoyal03@users.noreply.github.com>
2020-05-16 11:53:41 +0530

    script changes

--- scriptformovingman.js
+++ scriptformovingman.js
@@ -143,4 +143,36 @@ window.onload = function () {
     )
     .time(0.5);
   /* a Pen by Diaco m.lotfollahi : https://diacodesign.com */
+
+  let firstbtn = document.getElementById("year1btn");
+  let secondbtn = document.getElementById("year2btn");
+  let thirdbtn = document.getElementById("year3btn");
+  var year1link = document.getElementById("year1link");
+  var year2link = document.getElementById("year2link");
+  var yaer3link = document.getElementById("year3link");
+
+  firstbtn.onclick = function () {
+    var style = year1link.style.display;
+    if (style == "none") {
+      year1link.style.display = "block";
+    } else {
+      year1link.style.display = "none";
+    }
+  };
+  secondbtn.onclick = function () {
+    var style = year2link.style.display;
+    if (style == "block") {
+      year2link.style.display = "none";
+    } else {
+      year2link.style.display = "block";
+    }
+  };
+  thirdbtn.onclick = function () {
+    var style = year3link.style.display;
+    if (style == "block") {
+      year3link.style.display = "none";
+    } else {
+      year3link.style.display = "block";
+    }
+  };
 };
